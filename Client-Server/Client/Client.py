6abc804bd0f08a388df797fc12bb0088a9bf4bfd
Redon Osmanollaj <redon_osmanollaj@hotmail.com>
2019-04-05 16:24:03 +0200

    Sockets created

--- Client-Server/Client/Client.py
+++ Client-Server/Client/Client.py
@@ -1,1 +1,28 @@
+import socket
+
+serverName = '127.0.0.1'
+port = 1200
+
+clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+clientSocket.connect((serverName,port))
+
+varInput = input("Shkruaj nje fjali per serverin")
+
+clientSocket.sendall(str.encode(varInput))
+
+varReceive = clientSocket.recv(1024)
+
+print('Nga serveri ka ardhur: ',varReceive.decode())
+
+#varInput2 = input("Shkruaj nje fjali tjeter per serverin")
+#clientSocket.sendall(str.encode(varInput2))
+#varReceive2 = clientSocket.recv(1024)
+#print('Nga serveri ka ardhur: ',varReceive2.decode())
+
+
+clientSocket.close()
+
+
+
 
--- Client-Server/Server/Server.py
+++ Client-Server/Server/Server.py
@@ -1,1 +1,32 @@
+from socket import*
+
+serverName = 'localhost'
+port = 1200
+
+serverSocket = socket(AF_INET,SOCK_STREAM)
+serverSocket.bind((serverName,port))
+
+print('Serveri eshte duke degjuar ne portin ',str(port))
+
+serverSocket.listen(5)
+
+print('Serveri eshte duke pritur ndonje kerkese')
+
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    clientip, cport = connectionSocket.getpeername()
+    print('Client ip is: ',clientip)
+    print('Klienti u lidh ne serverin %s ne portin %s' %addr)
+    
+    fjalia = connectionSocket.recv(1024)
+    print('Serveri ka marre nga klienti fjaline: ',fjalia.decode())
+
+    fjaliaM = fjalia.upper()
+    connectionSocket.send(fjaliaM)
+    print('Dhe ka kthyer si pergjigje fjaline: ',fjaliaM.decode())
+    
+
+    connectionSocket.close()
+
 
