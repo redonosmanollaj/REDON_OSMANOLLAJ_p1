66c6d8e106c846db2842e4347d068130eec3ca17
Redon Osmanollaj <redon_osmanollaj@hotmail.com>
2019-04-16 08:47:00 +0200

    Port:12000

--- Client-Server(TCP)/Client/Client.py
+++ Client-Server(TCP)/Client/Client.py
@@ -1,18 +1,13 @@
 import socket
 
 serverName = '127.0.0.1'
-port = 1200
+port = 12000
 
 clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 clientSocket.connect((serverName,port))
 
 while True:
-  #  if varInput == "PERFUNDO":
-  #      break
-   # varInput = input("Operacioni (IPADRESA, NUMRIIPORTIT, BASHKETINGELLORE, PRINTIMI, EMRIIKOMPJUTERIT, KOHA, LOJA, FIBONACCI, KONVERTIMI, PERFUNDO)?\n")
-    
-    #varInput = input("")
     varReceive = clientSocket.recv(1024)
     varReceive = varReceive.decode()
     print(varReceive) 
